3ec5bf5f5fae34906efac0436d0617d40cb404ec
Sergey Zavitaev <nikgala@list.ru>
2023-06-22 21:56:54 +0300

    Added solutio for task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 10000 == number % 10 &
+    (number / 1000) % 10 == (number % 100) / 10)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
