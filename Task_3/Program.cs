4ee2439117759843f6d93f807e385f32ed4754cc
Sergey Zavitaev <nikgala@list.ru>
2023-06-22 22:56:03 +0300

    Added solution for task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+int[] ResultArray = new int[Convert.ToInt32(Console.ReadLine())];
+for (int i = 1; i < (ResultArray.Length + 1); i++) //Цикл по каждому элементу для замены
+    ResultArray[i - 1] = (i * i * i); //Само умножение
+Console.WriteLine(string.Join(" ", ResultArray));
