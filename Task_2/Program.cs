4fc79c7bf914653448bd6b00deb36b60c131e6ad
Sergey Zavitaev <nikgala@list.ru>
2023-06-22 22:04:43 +0300

    Added solutio for task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите массив x1 y1 z1 x2 y2 z2: ");
+int[] coords = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+double result = Math.Sqrt(
+    (coords[0] - coords[3]) * (coords[0] - coords[3])
+    + (coords[1] - coords[4]) * (coords[1] - coords[4])
+    + (coords[2] - coords[5]) * (coords[2] - coords[5]));
+
+Console.WriteLine(result);
